34b3888bcb31f6f1628c6262aab91f0b20db01f5
AdonadoMolinares <donadomolinares@gmail.com>
2025-10-25 05:01:26 -0500

    commit

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,9 +5,101 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 6
-      h1 Tema 6
+      h1 Valor agregado y paquete de beneficios
+    
+    .row.justify-content-center.align-items-center.mb-0
+      .col-12.col-lg-10(data-aos="zoom-in-right")
+        .cita-decorada
+          blockquote.cita-decorada__caja.cita-decorada__caja--fondo-2.p-4
+            .row.justify-content-center.align-items-center
+              .col-10.col-lg-10
+                p.cita-decorada__texto.mb-0 En los negocios verdes, el valor no está solo en el producto o servicio principal, sino también en todo lo que se construye alrededor de él. El valor agregado es clave para captar clientes conscientes del impacto ambiental y social.
+              .col-2.col-lg-2.d-none.d-lg-block
+                figure
+                  img(src='@/assets/curso/temas/tema_6/img_2.png', class="cita-decorada__imagen-flotante-1")
 
+    separador
 
+    .bg_img_2.mb-3
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-10 
+          #t_6_1.titulo-segundo.color-acento-contenido
+            .h2(data-aos="fade-right") 6.1.	Concepto
+          .mb-3
+
+      .row.justify-content-center.align-items-center.mb-3
+        .col-2.col-lg-3.d-none.d-lg-block
+          figure
+            img(src='@/assets/curso/temas/tema_6/img_3.png', class="cita-decorada__imagen-flotante-1")
+        .col-12.col-lg-7.mb-lg-0.mb-3
+          p.mb-3 El valor agregado consiste en incorporar cualidades adicionales al producto o servicio, que lo hacen más atractivo, útil o valioso para el cliente. Estos atributos pueden estar relacionados con el cuidado del medioambiente, la innovación, la salud del consumidor, la comunidad o el diseño.
+          p.mb-3 El paquete de beneficios representa el conjunto completo de ventajas que una persona recibe al comprar un producto o servicio. No solo se refiere a lo que se lleva físicamente, sino a la experiencia, garantía, valores de marca, sostenibilidad, atención al cliente y más.
+          .bloque-texto-b.color-secundario-10.p-4
+            .bloque-texto-b__texto
+              i.fas.fa-quote-left
+              h4.mb-3.ps-3 El paquete total de beneficios que ofrece una empresa es lo que realmente consume el cliente, no solo el producto físico 
+              i.fas.fa-quote-right
+          p.mb-0.text-end Kotler &amp; Armstrong, 2017.
+
+      separador
+
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-10 
+          #t_6_2.titulo-segundo.color-acento-contenido
+            .h2(data-aos="fade-right") 6.2. Estructura
+          .mb-3
+
+      .row.justify-content-center.align-items-center.mb-5
+          .col-8.col-lg-1.mb-lg-0.d-none.d-lg-block.mb-3(data-aos="zoom-in-right")
+            figure
+              img(src="@/assets/curso/temas/tema_6/img_4.png", class="img-fluid")
+          .col-12.col-lg-9(data-aos="zoom-in-right")
+            p.mb-3 El paquete de beneficios que ofrece un producto puede analizarse en tres niveles. Cada nivel aporta valor al cliente, desde lo más básico hasta lo más elaborado. Comprender estos niveles permite a los negocios verdes diseñar ofertas más completas, atractivas y coherentes con sus principios sostenibles. Estos niveles son: 
+
+      separador
+      .row.justify-content-center.align-items-center.mb-0
+        .col-12.col-lg-10 
+          #t_6_3.titulo-segundo.color-acento-contenido
+            .h2(data-aos="fade-right") 6.3.	Aplicabilidad
+          .mb-3
+      
+      .row.justify-content-center.align-items-star.mb-0
+        .col-8.col-lg-2.mb-lg-0.mb-3(data-aos="zoom-in-right")
+          figure
+            img(src="@/assets/curso/temas/tema_6/img_5.png", class="img-fluid").h-100
+        .col-12.col-lg-8(data-aos="zoom-in-right")
+          p.mb-3 Para que el modelo TPDP (Producto – Diferenciador – Beneficio para el cliente) funcione de manera efectiva en negocios verdes, es necesario que esté alineado con los principios de sostenibilidad y conciencia ambiental. No se trata solo de vender, sino de generar valor con propósito. A continuación, se presentan algunas recomendaciones clave para aplicar este enfoque de forma coherente y exitosa en el contexto de los negocios verdes. 
+            .row.justify-content-center.align-items-center
+              .col-12.col-lg-12
+                TabsA.mb-0.tarjeta-tabs-a.color-acento-botones
+                  .tarjeta.color-acento-contenido-5.p-3.h-100.tarjeta-tabs-a__tarjeta(titulo="Conocer al cliente verde")
+                    .row.justify-content-center.align-items-center.mb-0
+                      .col-12.col-sm-8.col-md-8.col-lg-8.col-xl-8.col-xxl-8.mb-4.mb-xl-0
+                        p.mb-0 identificar qué valores y causas sociales o ambientales valora el público objetivo.
+                      .col-8.col-sm-2.col-md-3.col-lg-3.col-xl-2.col-xxl-2.mb-4.mb-xl-0
+                        figure
+                          img(src='@/assets/curso/temas/tema_6/img_6.svg', class="img-fluid")(data-aos="fade-up")
+                  .tarjeta.color-acento-contenido-5.p-3.h-100.tarjeta-tabs-a__tarjeta(titulo="Construir una propuesta única")
+                    .row.justify-content-center.align-items-center.mb-0
+                      .col-12.col-sm-8.col-md-8.col-lg-8.col-xl-8.col-xxl-8.mb-4.mb-xl-0
+                        p.mb-0 combinar calidad, sostenibilidad y experiencia para diferenciar el producto.
+                      .col-8.col-sm-2.col-md-3.col-lg-3.col-xl-2.col-xxl-2.mb-4.mb-xl-0
+                        figure
+                          img(src='@/assets/curso/temas/tema_6/img_7.svg', class="img-fluid")(data-aos="fade-up")
+                  .tarjeta.color-acento-contenido-5.p-3.h-100.tarjeta-tabs-a__tarjeta(titulo="Comunicar con transparencia")
+                    .row.justify-content-center.align-items-center.mb-0
+                      .col-12.col-sm-8.col-md-8.col-lg-8.col-xl-8.col-xxl-8.mb-4.mb-xl-0
+                        p.mb-0 mostrar claramente los beneficios ambientales y sociales, con evidencia cuando sea posible (certificaciones, etiquetas, trazabilidad).
+                      .col-8.col-sm-2.col-md-3.col-lg-3.col-xl-2.col-xxl-2.mb-4.mb-xl-0
+                        figure
+                          img(src='@/assets/curso/temas/tema_6/img_8.svg', class="img-fluid")(data-aos="fade-up")
+                  .tarjeta.color-acento-contenido-5.p-3.h-100.tarjeta-tabs-a__tarjeta(titulo="Incluir elementos de educación")
+                    .row.justify-content-center.align-items-center.mb-0
+                      .col-12.col-sm-8.col-md-8.col-lg-8.col-xl-8.col-xxl-8.mb-4.mb-xl-0
+                        p.mb-0 aportar contenido que forme conciencia ecológica en el consumidor, como guías de reciclaje o información sobre impactos positivos del producto.
+                      .col-8.col-sm-2.col-md-3.col-lg-3.col-xl-2.col-xxl-2.mb-4.mb-xl-0
+                        figure
+                          img(src='@/assets/curso/temas/tema_6/img_9.svg', class="img-fluid")(data-aos="fade-up")
 </template>
 
 <script>
